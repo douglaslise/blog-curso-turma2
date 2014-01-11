802494bef46980b3b14708d2b007aa74baf90fe2
Douglas <douglaslise@gmail.com>
2014-01-11 12:37:21 -0200

    Aula 3

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140110234414) do
+
+  create_table "posts", force: true do |t|
+    t.string   "titulo"
+    t.text     "texto"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
